b4f164ef889ce18f5b1fca095375c37f4c80f0aa
Jeremy <gojerjeremy@gmail.com>
2023-08-27 18:17:50 +0530

    Final changes by me

--- reactapp/reactapp/New folder/src/LogOut.js
+++
@@ -1,8 +0,0 @@
-import { useNavigate } from "react-router-dom";
-
-
-export default function LogOut(){
-    const navigate = useNavigate();
-    localStorage.removeItem("UserSession")
-    navigate("/")
-} 
--- reactapp/reactapp/New folder/src/PrivateRoute/PrivateRoute.js
+++
@@ -1,7 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-
-export default function PrivateRoute({children}){
-    const getSession = localStorage.getItem('UserSession')
-    return getSession ? children : <Navigate to='/Login'/>
-}
